32b76379f8cb7429bce61811a50e9063cc1a0d0a
Matthew Branning <48932625+juandap1@users.noreply.github.com>
2023-11-27 23:47:02 -0500

    Basic functionality of final level working, needs to be touched up on

--- Assets/Scripts/FollowWaypoint.cs
+++ Assets/Scripts/FollowWaypoint.cs
@@ -5,19 +5,27 @@ using UnityEngine.UI;
 
 public class FollowWaypoint : MonoBehaviour
 {
+    [Header("Track Settings")]
     public GameObject track;
     int currentWaypoint = 0;
 
+    [Header("Traversal Settings")]
     public float speed = 10f;
     public float rotSpeed = 10f;
     public bool loop = false;
-
     public float sideMovement = 0f;
+    public float maxHorizontalDisplacement = 10f;
     public Transform sled;
+
+    [Header("Gameplay Settings")]
     public Slider progress;
+    public GameObject winScene;
+    public GameObject UI;
+    public GameObject loseScene;
 
     List<Transform> waypoints = new List<Transform>();
     bool finished = false;
+    bool lost = false;
     float distToNext;
 
     // Start is called before the first frame update
@@ -28,12 +36,13 @@ public class FollowWaypoint : MonoBehaviour
             waypoints.Add(child);
         }
         distToNext = Vector3.Distance(transform.position, waypoints[currentWaypoint].position);
+        UI.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!finished)
+        if (!finished && !lost)
         {
             progress.value = Mathf.Max(currentWaypoint - 1, 0) / (float)waypoints.Count;
             if (Vector3.Distance(transform.position, waypoints[currentWaypoint].position) < 3)
@@ -53,7 +62,29 @@ public class FollowWaypoint : MonoBehaviour
             transform.Translate(0, 0, speed * Time.deltaTime);
 
             float h = sideMovement * Input.GetAxis("Horizontal");
-            sled.localPosition = new Vector3(Mathf.Clamp(sled.localPosition.x + (h * Time.deltaTime), -7f, 7f), 0, 0);
+            sled.localPosition = new Vector3(Mathf.Clamp(sled.localPosition.x + (h * Time.deltaTime), -maxHorizontalDisplacement, maxHorizontalDisplacement), 0, 0);
         }
+        ManageUI();
+    }
+
+    void ManageUI()
+    {
+        if (finished)
+        {
+            UI.SetActive(false);
+            winScene.SetActive(true);
+        }
+        if (lost)
+        {
+            UI.SetActive(false);
+            loseScene.SetActive(true);
+        }
+    }
+
+    public void ActiveLoss()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        lost = true;
     }
 }
